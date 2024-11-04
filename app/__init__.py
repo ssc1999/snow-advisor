eef4f210bd515b7e2fcbc3acf563a3355227487b
Sergio Salgado <ssc1099.csmm@gmail.com>
2024-11-04 20:48:17 +0100

    feat: basic Flask files added to correctly run the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
+# app/__init__.py
 from flask import Flask
+# Import and register blueprints
+from .api.routes import bp as api_bp
 
 def create_app():
     app = Flask(__name__)
-
-    from .routes import main
-    app.register_blueprint(main)
+    app.register_blueprint(api_bp, url_prefix="/api")  # Prefix set to /api
 
     return app
-

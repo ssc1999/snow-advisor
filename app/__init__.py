4da3b0cd66d8ad9e7535535076cd513a441103a5
Sergio Salgado <ssc1099.csmm@gmail.com>
2024-11-08 02:07:50 +0100

    feat: CORS added

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 # app/__init__.py
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 # Import and register blueprints
 from .api.routes import bp as api_bp
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(api_bp, url_prefix="/api")  # Prefix set to /api
+    CORS(app)  # Apply CORS to the app
+
+    # Register the blueprint with a URL prefix
+    app.register_blueprint(api_bp, url_prefix="/api")
 
     return app

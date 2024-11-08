daaa0d8ab7efb9af4d05b63f8cd61b2634377835
Sergio Salgado <ssc1099.csmm@gmail.com>
2024-11-08 02:07:32 +0100

    refactor: saving data in database updated

--- app/db/mongodb.py
+++ app/db/mongodb.py
@@ -1,7 +1,7 @@
 # app/db/mongodb.py
-import os
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING
 from datetime import datetime
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,12 +9,26 @@ client = MongoClient(os.getenv("MONGODB_URI"))
 db = client["snow_advising"]
 collection = db["daily_data"]
 
-def save_daily_data(data):
-    # Add a timestamp for when the data is stored
+# Ensure resort_name is unique
+collection.create_index([("resort_name", ASCENDING)], unique=True)
+
+def save_daily_data(resort_name, data):
+    # Remove existing "date" field if present to avoid creating duplicates on update
+    data.pop("date", None)
+    # Add current timestamp only if we’re inserting fresh data
     data["date"] = datetime.utcnow().strftime("%Y-%m-%d")
-    # Find and replace the data with the same resort name
-    collection.find_one_and_replace(resort_name, data)
+    
+    # Upsert by resort_name, replace if exists, or insert new if not
+    collection.find_one_and_replace(
+        {"resort_name": resort_name},  # Filter by resort name
+        data,                           # Data to replace or insert
+        upsert=True                     # Insert if not found
+    )
 
 def get_daily_data(resort_name):
-    # Default to get the one with the resort name
-    return collection.find_one(resort_name)
+    # Retrieve the data from the database
+    data = collection.find_one({"resort_name": resort_name})
+    if data:
+        # Remove the `_id` field as it is not JSON serializable
+        data.pop("_id", None)
+    return data

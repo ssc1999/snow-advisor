4c4d36f733f81abf1fcdb4489add5a13d12bf55b
Sergio Salgado <ssc1099.csmm@gmail.com>
2024-11-12 01:23:33 +0100

    refactor: cron updated

--- app/cron/daily_scraper.py
+++ app/cron/daily_scraper.py
@@ -2,18 +2,25 @@
 from scraper.snow_forecast import SnowForecastScraper
 from scraper.infonieve import InfonieveScraper
 from processor.data_processor import DataProcessor
-from db.mongodb import save_daily_data
+from db.mongodb import save_daily_data, get_resorts_cache
 
 def daily_scrape():
-    snow_forecast_scraper = SnowForecastScraper()
-    infonieve_scraper = InfonieveScraper()
-    processor = DataProcessor()
-    snow_forecast_raw_data = snow_forecast_scraper.scrape_snow_forecast()
-    infonieve_raw_data = infonieve_scraper.scrape_infonieve()
-    processed_data = processor.process_data(snow_forecast_raw_data, infonieve_raw_data)
-
-    # Save processed data to MongoDB
-    save_daily_data(processed_data)
+    resorts = get_resorts_cache()
+    for resort in resorts:
+        resort_name = resort["resort_name"]
+        snow_forecast_scraper = SnowForecastScraper(resort_name)
+        infonieve_scraper = InfonieveScraper(resort_name)
+        processor = DataProcessor()
+        snow_forecast_raw_data = snow_forecast_scraper.scrape_weather()
+        infonieve_raw_data = infonieve_scraper.scrape_resort_data()
+        processed_data = processor.process_data(resort_name, snow_forecast_raw_data, infonieve_raw_data)
+        
+        if processed_data:
+        # Attempt to save; only proceed if save is successful
+            if not save_daily_data(resort_name, processed_data):
+                return jsonify({"error": f"Data not found or could not scrape data for {resort_name}"}), 404
 
 if __name__ == "__main__":
     daily_scrape()
+# TODO add a cron job to run this every day at midnight
+# TODO add logger

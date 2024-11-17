b0829d069fc359eb84a26c023bc325eecbef5063
Sergio Salgado <ssc1099.csmm@gmail.com>
2024-11-17 18:23:42 +0100

    fix: cron scrapper name resolver for each resort

--- app/cron/daily_scraper.py
+++ app/cron/daily_scraper.py
@@ -2,17 +2,21 @@
 from scraper.snow_forecast import SnowForecastScraper
 from scraper.infonieve import InfonieveScraper
 from processor.data_processor import DataProcessor
-from db.mongodb import save_daily_data, get_resorts_cache
+from db.mongodb import save_daily_data, get_resorts_cache, check_resort_in_all_resorts
 
 def daily_scrape():
     resorts = get_resorts_cache()
     for resort in resorts:
-        resort_name = resort["resort_name"]
-        infonieve_name = resort["infonieve_name"]
-        snow_forecast_name = resort["snow_forecast_name"]
+        resort_data = check_resort_in_all_resorts(resort["resort_name"])
+        
+        resort_name = resort_data["resort_name"]
+        infonieve_name = resort_data["infonieve_name"]
+        snow_forecast_name = resort_data["snow_forecast_name"]
+        
         infonieve_scraper = InfonieveScraper(infonieve_name)
         snow_forecast_scraper = SnowForecastScraper(snow_forecast_name)
         processor = DataProcessor()
+        
         infonieve_raw_data = infonieve_scraper.scrape_resort_data()
         snow_forecast_raw_data = snow_forecast_scraper.scrape_weather()
         processed_data = processor.process_data(self,resort_name, snow_forecast_raw_data, infonieve_raw_data)
